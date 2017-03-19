1e1f0da9d88de013ba578d39b4e58437ebd4a092
William Fichtner <william_25_35@hotmail.com>
2017-03-19 15:12:24 -0600

    started

---
+++ CMPE1600_Lab4/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace CMPE1600_Lab4
+{
+    public partial class Form1 : Form
+    {
+        CDrawer canvas = new CDrawer();
+        byte[ , ] backgroundArr = new byte[80, 60];
+        byte[,] foregroundArr = new byte[80, 60];
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas.Scale = 10;
+        }
+    }
+}
